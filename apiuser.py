94d90dc18d874d7cdd86b4af8aa2413bbef1528b
kebosatu <33442100+kebosatu@users.noreply.github.com>
2019-10-29 22:31:25 +0700

    Add files via upload

--- apiuser.py
+++ apiuser.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import logging
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='../mntlog/app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.warning('This will get logged to a file')
 
 
